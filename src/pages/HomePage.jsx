efd8eb8b3660a5b9690bebde7c3fb4380ca4eea5
omri gendler <omrig999@gmail.com>
2025-09-28 17:31:04 +0300

    feat: implement MusicPlayerSlider component in HomePage for enhanced audio playback experience

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
+import MusicPlayerSlider from '../cmps/MusicPlayer.jsx'
+
 export function HomePage() {
     return (
         <section className="home">
-            <h1>Home sweet Home</h1>
+            <MusicPlayerSlider />
         </section >
     )
 }
