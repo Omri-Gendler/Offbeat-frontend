13a7a4d3d16c5da54be5b0ca6f76bc1f6322dee4
omri gendler <omrig999@gmail.com>
2025-09-28 17:25:06 +0300

    feat: add ContinuousSlider component to AppFooter for enhanced user interaction

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux'
 
+import ContinuousSlider from './Slider.jsx'
+
 export function AppFooter() {
 	const count = useSelector(storeState => storeState.userModule.count)
 
 	return (
 		<footer className="app-footer full">
+			<ContinuousSlider />
 			<p>Coffeeright, Liat & Omri &copy;  </p>
 		</footer>
 	)
