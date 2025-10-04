d1c1f4ee9f8dabd46ae6ef1108a1f4c08daabc0e
omri gendler <omrig999@gmail.com>
2025-10-04 21:52:08 +0300

    style: refine StationList and StationDetails styles for improved layout and responsiveness

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -53,11 +53,11 @@ export function StationList({ stations, onRemoveStation, onUpdateStation }) {
             <ul className="station-list">
                 {stations.map(station =>
                     <li key={station._id}>
-                        {<img className="station-img" src={station.imgUrl} alt="" style={{ height: '160px', width: '100%', objectFit: 'cover' }} />}
+                        {<img className="station-img" src={station.imgUrl} alt="" />}
                         <StationPreview station={station} />
                         {<div className="actions">
                             {/* <button onClick={() => onUpdateStation(station)}>Edit</button> */}
-                            <button onClick={() => onRemoveStation(station._id)}>x</button>
+                            {/* <button onClick={() => onRemoveStation(station._id)}>x</button> */}
                         </div>}
                     </li>
                 )}
