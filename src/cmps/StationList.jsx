63e8703af13e8f9f3260b2bc788dab93832744ad
omri gendler <omrig999@gmail.com>
2025-10-11 14:46:15 +0300

    add play button to station list and left sidebar; improve button styles and hover effects

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -62,17 +62,23 @@ export function StationList({ stations, onRemoveStation, onUpdateStation }) {
                 <h2 className='station-list-title'>All Stations</h2>
 
                 <ul className="station-list">
-                    {stations.map(station =>
-                        <li key={station._id}>
+                    {stations.map(station => (
+                        <li key={station._id} className="station-list-wrapper">
                             <StationPreview station={station} />
-                            {<div className="actions">
+                            <div className="play-button">
+                                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8 5V19L19 12L8 5Z" fill="black" />
+                                </svg>
+                            </div>
+                            <div className="actions">
                                 {/* <button onClick={() => onUpdateStation(station)}>Edit</button> */}
                                 {/* <button onClick={() => onRemoveStation(station._id)}>x</button> */}
-                            </div>}
+                            </div>
+
                         </li>
-                    )}
+                    ))}
                 </ul>
             </div >
-        </section>
+        </section >
     )
 }
